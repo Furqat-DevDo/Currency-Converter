4bc1f4a8ad389411ce5d5b2047a970677e62af03
Furqat-DevDo <furqata93@gmail.com>
2022-06-15 15:00:28 +0500

    Converter API is on the first stage

---
+++ ConverterAPI/Controllers/CurrensyController.cs
@@ -0,0 +1,12 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConverterAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurrensyController:Controller
+    {
+        
+    }
+}
